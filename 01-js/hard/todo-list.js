7aa98aa0157f01263175a7af73ecdced5b721d2d
Viveak Kudesia <71117587+viveakkudesia@users.noreply.github.com>
2024-08-25 01:01:10 +0530

    Updated todo-list.js

--- 01-js/hard/todo-list.js
+++ 01-js/hard/todo-list.js
@@ -11,7 +11,47 @@
 */
 
 class Todo {
+  constructor() {
+    this.list = []; // Initialize an empty array to store todos
+  }
 
+  // Method to add a new todo to the list
+  add(todo) {
+    this.list.push(todo);
+  }
+
+  // Method to remove a todo by index
+  remove(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.list.length) {
+      this.list.splice(indexOfTodo, 1);
+    }
+    // If index is invalid, do nothing
+  }
+
+  // Method to update a todo at a given index
+  update(index, updatedTodo) {
+    if (index >= 0 && index < this.list.length) {
+      this.list[index] = updatedTodo;
+    }
+    // If index is invalid, do nothing
+  }
+
+  // Method to get all todos
+  getAll() {
+    return this.list;
+  }
+
+  // Method to get a specific todo by index
+  get(indexOfTodo) {
+    if (indexOfTodo >= 0 && indexOfTodo < this.list.length) {
+      return this.list[indexOfTodo];
+    }
+    return null; // Return null if the index is invalid
+  }
+
+  // Method to clear all todos
+  clear() {
+    this.list = [];
+  }
 }
-
 module.exports = Todo;
